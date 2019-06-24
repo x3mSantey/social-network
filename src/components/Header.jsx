ab99e907681d471f93e035ccc16b2decf488dd0e
Santey <nomadsantey21@mail.ru>
2019-06-24 17:25:14 +0300

    first step

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src='https://i.ya-webdesign.com/images/sony-logo-png-7.png'/>
+        </header>
+    )
+}
+
+export default Header
